a363b366254db7be1c9299161d1c0c9e2dc21aca
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2022-09-16 10:23:12 -0700

    Code

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Logo from './img/logo.png';
+
+const Header = () => {
+  return (
+    <header className='fixed z-50 w-screen  p-6 px-16'>
+        {/* desktop & tablet */}
+
+        <div className='hideen md:flex w-full h-full'>
+             <div className='flex items-center gap-2'>
+                <img src={Logo} className="w-8 object-cover" alt='logo'/>
+                <p className='text-headingColor text-xl font-bold'>City</p>
+           </div>
+
+           <ul className='flex items-center gap-8 ml-auto'>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About us</li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Service</li>
+           </ul>
+        </div>
+
+{/*mobile */}
+ <div className='flex md:hidden w-full h-full'></div>
+</header>
+  )
+}
+
+export default Header
